2dd856775d6132e80fd2683d8eceff1e3141cae0
dailyghibli <dailyghibli@users.noreply.github.com>
2018-03-11 19:11:39 +0100

    Implemented application logic
    Implemented method buttonCheckOnClick, which gets called, when user clicks the Check button.
    
    Input is first tested, if it's long enough and if yes, it is further checked by isPalindrome method.

--- app/src/main/java/com/example/emi/palindromeapp/MainActivity.java
+++ app/src/main/java/com/example/emi/palindromeapp/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.emi.palindromeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editInputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Resources.id.editInputText
+
+        editInputText = findViewById(R.id.editInputText);
+    }
+
+    public void buttonCheckOnClick(View view) {
+        String text = editInputText.getText().toString();
+
+        if(text.length() == 0)
+            Toast.makeText(this, "You must enter some text!", Toast.LENGTH_SHORT).show();
+        else if(text.length() < 5)
+            Toast.makeText(this, "You must enter text at least 5 characters long!", Toast.LENGTH_SHORT).show();
+        else
+        {
+            if(StringUtility.isPalindrome(text))
+                Toast.makeText(this, "Input '" + text + "' is a palindrome!", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "Input '" + text + "' is not a palindrome!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
